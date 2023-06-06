bca824a07bf899ffa8cae83b6efdfbaeb2bd4720
Anna Russi <annarussi@gmail.com>
2023-06-06 13:17:59 +0100

    did the new and create methods for the kids controller and simple form in hte new view

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,2 +1,25 @@
 class KidsController < ApplicationController
+
+  def new
+    @kid = Kid.new
+  end
+
+  def create
+    @kid = Kid.new(kid_params)
+    @kid.user = current_user
+    # We have to update the avatar for the real one
+    @kid.avatar = Avatar.new(name: "moana")
+
+    if @kid.save
+      redirect_to kids_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kid_params
+    params.require(:kid).permit(:nickname, :age)
+  end
 end
