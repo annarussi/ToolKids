8c9d8378e48276287befde2aadf1b6db1469bf1a
Betina Pinto <betina.correap@gmail.com>
2023-06-07 12:54:34 +0100

    added audios to the colors

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,7 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
 //= link vermelho.mp3
+//= link vermelho_win.mp3
+//= link azul.mp3
+//= link amarelo.mp3
+//= link verde.mp3
--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -2,18 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="game"
 export default class extends Controller {
-  static targets = ["vermelho"]
+  static targets = ["vermelho", "win", "azul", "amarelo", "verde"]
 
   connect() {
     console.log("game is here")
     this.vermelhoTarget.load();
+    this.winTarget.load();
+    this.azulTarget.load();
   }
 
   playSound() {
     this.vermelhoTarget.play()
   }
 
-  playSound(){
+  playWin(){
+    this.winTarget.play()
+  }
 
+  playAzul() {
+    this.azulTarget.play()
+  }
+
+  playAmarelo() {
+    this.amareloTarget.play()
+  }
+
+  playVerde() {
+    this.verdeTarget.play()
   }
 }
