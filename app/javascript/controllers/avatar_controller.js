9c1e548cf9c962b9d7640751d906a54405fba4f1
Anna Russi <annarussi@gmail.com>
2023-06-08 21:33:35 +0100

    setup the avatar stimulus controller and add the data controller to the edit view

--- app/javascript/controllers/avatar_controller.js
+++ app/javascript/controllers/avatar_controller.js
@@ -2,7 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="avatar"
 export default class extends Controller {
-  static targets = []
+  static targets = ["avatarId"]
   connect() {
+
+    console.log("avatar controller here");
+
   }
 }
