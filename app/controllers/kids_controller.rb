30dc4db66d96c439d650649ff4d9be26d4c85a2b
Anna Russi <annarussi@gmail.com>
2023-06-10 16:52:17 +0100

    setup the kid create action to create the games for each kid

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class KidsController < ApplicationController
   before_action :set_kids, only: %i[show destroy edit moana_avatar pocahontas_avatar mogli_avatar tarzan_avatar]
   protect_from_forgery with: :null_session
@@ -15,11 +17,42 @@ class KidsController < ApplicationController
 
   def create
     @kid = Kid.new(kid_params)
+
+    # Give a parent for the kid
     @kid.user = current_user
-    # We have to update the avatar for the real one
+
+    # Give an avatar for the kid
     @kid.avatar = Avatar.last
 
     if @kid.save
+
+      # Create the Color game for the kid
+      game_colors = Game.new(name: "colors", level: 1, kid: @kid)
+      game_colors.save
+      file = URI.open("https://res.cloudinary.com/dhku8hlz9/image/upload/v1686224730/ci66vxhb0wpbexursb6m.png")
+      game_colors.photos.attach(io: file, filename: "blue.png", content_type: "image/png")
+
+      file = URI.open("https://res.cloudinary.com/dhku8hlz9/image/upload/v1686224762/vs3k9plgsh2cclepgze8.png")
+      game_colors.photos.attach(io: file, filename: "yellow.png", content_type: "image/png")
+
+      file = URI.open("https://res.cloudinary.com/dhku8hlz9/image/upload/v1686224779/pusmec8ipokomlwmmxqc.png")
+      game_colors.photos.attach(io: file, filename: "green.png", content_type: "image/png")
+
+      file = URI.open("https://res.cloudinary.com/dhku8hlz9/image/upload/v1686224795/y7dxvwjwh6j1xfsks1iw.png")
+      game_colors.photos.attach(io: file, filename: "red.png", content_type: "image/png")
+
+      # Create the Animal game for the kid
+      game_animals = Game.new(name: "animals", level: 2, kid: @kid)
+      game_animals.save
+
+      # Create the Objects game for the kid
+      game_objects = Game.new(name: "objects", level: 3, kid: @kid)
+      game_objects.save
+
+      # Create the Verbs game for the kid
+      game_verbs = Game.new(name: "verbs", level: 4, kid: @kid)
+      game_verbs.save
+
       redirect_to edit_kid_path(@kid)
     else
       render :new, status: :unprocessable_entity
@@ -83,9 +116,6 @@ class KidsController < ApplicationController
     @kid.destroy
     redirect_to kids_path
   end
-
-
-
 
   private
 
