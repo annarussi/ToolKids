71b3ac029eec75d6aca0ea325656c6d4a31cbfb4
Anna Russi <annarussi@gmail.com>
2023-06-14 12:32:29 +0100

    added animatiomn ot the new avatar award in the edit view

--- app/javascript/controllers/game_controller.js
+++ app/javascript/controllers/game_controller.js
@@ -5,7 +5,7 @@ import { Turbo } from "@hotwired/turbo-rails";
 
 // Connects to data-controller="game"
 export default class extends Controller {
-  static targets = ["gameId", "kidId", "exercise", "win", "wrongAnswer1", "wrongAnswer2", "wrongAnswer3", "wrongAnswer1Card", "wrongAnswer2Card", "wrongAnswer3Card"]
+  static targets = ["gameId", "kidId", "avatar", "exercise", "win", "wrongAnswer1", "wrongAnswer2", "wrongAnswer3", "wrongAnswer1Card", "wrongAnswer2Card", "wrongAnswer3Card"]
 
   connect() {
     console.log("game is here")
@@ -22,6 +22,8 @@ export default class extends Controller {
     const kidId = this.kidIdTarget.innerText.trim();
     const gameId = this.gameIdTarget.innerText.trim();
 
+    this.avatarTarget.classList.remove('d-none')
+
     this.winTarget.play()
 
     setTimeout(() =>
@@ -32,14 +34,14 @@ export default class extends Controller {
         if (response.ok) {
         // Redirecionamento para outra view
         Turbo.visit(`/kids/${kidId}/games`);
-      }}), 3000);
+      }}), 10000);
   }
 
 
   playWrongAnswer1() {
     this.wrongAnswer1Target.play()
     this.wrongAnswer1CardTarget.classList.add('game-card')
-    
+
     setTimeout(() =>
     this.wrongAnswer1CardTarget.classList.remove('game-card'), 1000);
   }
@@ -47,7 +49,7 @@ export default class extends Controller {
   playWrongAnswer2() {
     this.wrongAnswer2Target.play()
     this.wrongAnswer2CardTarget.classList.add('game-card')
-    
+
     setTimeout(() =>
     this.wrongAnswer2CardTarget.classList.remove('game-card'), 1000);
   }
@@ -55,7 +57,7 @@ export default class extends Controller {
   playWrongAnswer3() {
     this.wrongAnswer3Target.play()
     this.wrongAnswer3CardTarget.classList.add('game-card')
-    
+
     setTimeout(() =>
     this.wrongAnswer3CardTarget.classList.remove('game-card'), 1000);
 
