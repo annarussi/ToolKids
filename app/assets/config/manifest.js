4c3aeba6afb29ae31793914c5395b66ded5170b4
Anna Russi <annarussi@gmail.com>
2023-06-14 09:07:20 +0100

    added the audios files to the manifest.js file

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,3 +6,33 @@
 //= link azul.mp3
 //= link amarelo.mp3
 //= link verde.mp3
+
+// Anna's audios
+
+//= link A-Amarelo.mp3
+//= link A-Ariel.mp3
+//= link A-Azul.mp3
+//= link A-Elefante.mp3
+//= link A-Girafa.mp3
+//= link A-Leao-Parabens.mp3
+//= link A-Moana.mp3
+//= link A-Pocahontas.mp3
+//= link A-Tigre.mp3
+//= link A-Verde.mp3
+//= link A-Vermelho-parabens.mp3
+//= link A-Vermelho.mp3
+
+// Franciscos Audios
+
+//= link F-Azul.mp3
+//= link F-Elefante.mp3
+//= link F-Girafa.mp3
+//= link F-Leao-parabens.mp3
+//= link F-Leao.mp3
+//= link F-Tigre.mp3
+//= link F-Verde.mp3
+//= link F-Vermelho-parabens.mp3
+//= link F-Vermelho.mp3
+//= link Mogli.mp3
+//= link Simba.mp3
+//= link Tarzan.mp3
